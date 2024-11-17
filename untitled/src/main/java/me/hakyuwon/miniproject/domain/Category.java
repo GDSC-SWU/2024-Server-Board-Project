f7c48247a986bca3f5ccbbe1597fcd794e9b336e
hakyuwon <kyu10225@naver.com>
2024-11-17 19:34:39 +0900

    카테고리

---
+++ untitled/src/main/java/me/hakyuwon/miniproject/domain/Category.java
@@ -0,0 +1,22 @@
+package me.hakyuwon.miniproject.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id", updatable = false)
+    private Long categoryId;
+
+
+}
