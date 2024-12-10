e07edcc56b52059147fc2fca3813ab79f86dd9e9
hyojin425 <gywls5184@gmail.com>
2024-12-11 00:21:48 +0900

    feat: logout 추가

--- hyojin-jimin/board/src/main/java/com/example/board/controller/UserController.java
+++ hyojin-jimin/board/src/main/java/com/example/board/controller/UserController.java
@@ -1,19 +1,24 @@
 package com.example.board.controller;
 
+import com.example.board.config.jwt.TokenProvider;
 import com.example.board.dto.ArticleDto;
 import com.example.board.dto.UserDto;
 import com.example.board.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 public class UserController {
 
     private final UserService userService;
+    private final TokenProvider tokenProvider;
 
     @PostMapping("/api/signup")
     public ResponseEntity<UserDto.UserSignupResponseDto> signup(@RequestBody UserDto.UserSignupRequestDto request) {
@@ -25,5 +30,11 @@ public class UserController {
     public ResponseEntity<UserDto.UserLoginResponseDto> login(@RequestBody UserDto.UserLoginRequestDto request) {
         UserDto.UserLoginResponseDto loginResponse = userService.signIn(request);
         return ResponseEntity.ok().body(loginResponse);
+    }
+
+    @GetMapping("/api/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response){
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
+        return "redirect:/api/login";
     }
 }
